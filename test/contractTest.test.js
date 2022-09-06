5129c18a9627f93b4acb5c1ca042be84d239bdfe
Daniel Alisi <alisidaniel@gmail.com>
2022-09-06 23:36:14 +0200

    added test and a library

---
+++ test/contractTest.test.js
@@ -0,0 +1,9 @@
+const { expect } = require('chai');
+
+describe("Token contract", function () {
+    it("Hello test works", async function () {
+      const name = 'gameing contract';
+
+      expect(name).to.be.string
+    });
+  });
